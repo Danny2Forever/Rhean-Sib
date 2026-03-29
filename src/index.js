3eb9b33b2881b2c6bcefd17f741b5b81d62f9e44
Lattae <105736002+Danny2Forever@users.noreply.github.com>
2026-03-29 17:16:49 +0700

    feat: add roll command

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const { Client, IntentsBitField, Collection } = require('discord.js');
 
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.MessageContent,
-        IntentsBitField.Flags.GuildMembers,
     ],
 });
 
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith('!')) return;
+
+    const args = message.content.slice(1).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName);
+    if (!command) return;
+
+    command.execute(message, args);
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
