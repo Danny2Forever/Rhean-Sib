f3215d95bb125f9dc12fa257354bf41005534921
Lattae <105736002+Danny2Forever@users.noreply.github.com>
2026-03-29 16:46:16 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildMembers,
+    ],
+});
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
